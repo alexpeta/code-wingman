a11e8502a706e0b35e00e253e17ff824a9114bc3
unknown <alex@alexpeta.ro>
2016-01-17 01:23:55 +0200

    intial commit for client

---
+++ Client/src/bootstrap.ts
@@ -0,0 +1,6 @@
+/// <reference path="../typings/bundle.d.ts" />
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Main} from './main';
+ReactDOM.render(React.createElement(Main), document.getElementById('main'));
